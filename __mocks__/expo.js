d90ae4fc8c0066c065c242db0cb26e12c65c8664
Sean Cheong Zhen Xiong <sean21cheong@msn.com>
2023-07-27 22:52:17 +0900

    Setup jest unit test

---
+++ __mocks__/expo.js
@@ -0,0 +1,1 @@
+export const registerRootComponent = jest.fn();
---
+++ jest.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  preset: 'jest-expo',
+  setupFilesAfterEnv: ['@testing-library/jest-native/extend-expect'],
+  transform: {
+    '^.+\\.(js|jsx|ts|tsx)$': 'babel-jest'
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  moduleNameMapper: {
+    '^expo$': '<rootDir>/__mocks__/expo.js'
+  }
+};
