68f0256b58984356bf8a3cfa9764bf3b415355e7
Sean Cheong Zhen Xiong <sean21cheong@msn.com>
2023-07-27 22:14:35 +0900

    Move App.tsx into src folder

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { registerRootComponent } from 'expo';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
@@ -5,7 +6,7 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <StatusBar style='auto' />
     </View>
   );
 }
@@ -15,6 +16,8 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
-  },
+    justifyContent: 'center'
+  }
 });
+
+registerRootComponent(App);
